318633353aa4bedb177577e69c526395d2acc9b0
OleksiiTeplyi <oleksiy.tepliy@developex.net>
2025-01-04 17:02:51 -0500

    fix RTC wake up and stop mode enter/exit.

--- Core/Src/sleepManager.c
+++ Core/Src/sleepManager.c
@@ -10,7 +10,6 @@
 #include "keyscan.h"
 
 #define APM_MUTE_TIMEOUT_MS 250
-#define DEEP_SLEEP_MS 10000
 
 static uint32_t lastPlaybackMs = 0;
 static volatile bool rtcWakeupFlag = false;
@@ -41,13 +40,64 @@ void sleepManagerInit()
     ledOn();
     RTC_DateTypeDef  sDate = {0};
     RTC_TimeTypeDef sTime = {0};
-    HAL_RTC_SetDate(&hrtc, &sDate, RTC_FORMAT_BCD);
-    HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BCD);
+    HAL_RTC_SetDate(&hrtc, &sDate, RTC_FORMAT_BIN);
+    HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BIN);
+}
+
+static void rtcAlarmEnable(uint32_t sleepDurationMs)
+{
+    // RTC clock is 40 Khz, prescaler is set to 40, so second counter ticks each milisecond
+    RTC_AlarmTypeDef sAlarm = {0};
+    sAlarm.AlarmTime.Hours = sleepDurationMs / 3600;
+    sAlarm.AlarmTime.Minutes = (sleepDurationMs / 60) % 60;
+    sAlarm.AlarmTime.Seconds = sleepDurationMs % 60;
+    HAL_RTC_SetAlarm_IT(&hrtc, &sAlarm, RTC_FORMAT_BIN);
+}
+
+static void rtcAlarmDisable()
+{
+    HAL_RTC_DeactivateAlarm(&hrtc, RTC_ALARM_A);
+}
+
+static void enterStopMode()
+{
+    HAL_SuspendTick();
+    // Reset RTC time to zero
+    RTC_TimeTypeDef sTime = {0};
+    HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BIN);
+    uint32_t sleepDurationMs = 0;
+//    rtcAlarmEnable(); // Not used for now
+    ledOff();
+    // Enter Stop Mode
+    HAL_PWR_EnterSTOPMode(PWR_LOWPOWERREGULATOR_ON, PWR_STOPENTRY_WFI);
+    // Exit Stop Mode
+    ledOn();
+
+    // Update SysTick counter after sleep wake up
+    RTC_TimeTypeDef wakeUpTime = {0};
+    HAL_RTC_GetTime(&hrtc, &wakeUpTime, RTC_FORMAT_BIN);
+    uint32_t sleepMs = wakeUpTime.Hours * 3600 + wakeUpTime.Minutes * 60 + wakeUpTime.Seconds;
+    extern __IO uint32_t uwTick;
+    if (sleepMs < sleepDurationMs) {
+        uwTick += sleepDurationMs - sleepMs;
+    } else {
+        uwTick += sleepMs;
+    }
+
+    rtcAlarmDisable();
+    if (rtcWakeupFlag) {
+        rtcWakeupFlag = false;
+    }
+
+    // Enable SysTick and update clock configuration
+    extern void SystemClock_Config(void);
+    SystemClock_Config();
+    HAL_ResumeTick();
 }
 
 void sleepManagerProcess()
 {
-//  __disable_irq();
+    // __disable_irq();
     bool isSoundPlaying = notePlayerIsPlaying();
     bool isEffectPlaying = effectManagerIsPlaying();
     bool isKeyScanRunning = keyscanIsRunning();
@@ -60,54 +110,13 @@ void sleepManagerProcess()
         }
     }
 
-    // WIP for now
     __WFI();
-    return;
-
-    if (!isSoundPlaying && !isEffectPlaying && !isKeyScanRunning) {
-        HAL_SuspendTick();
-
-        // Reset RTC time to zero
-        RTC_TimeTypeDef sTime = {0};
-        HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BCD);
-
-        // RTC clock is 40 Khz, prescaler is set to 40, so second counter ticks each milisecond
-        RTC_AlarmTypeDef sAlarm = {0};
-        const uint32_t sleepDurationMs = DEEP_SLEEP_MS;
-        sAlarm.AlarmTime.Hours = sleepDurationMs / 3600;
-        sAlarm.AlarmTime.Minutes = (sleepDurationMs / 60) % 60;
-        sAlarm.AlarmTime.Seconds = sleepDurationMs % 60;
-        HAL_RTC_SetAlarm_IT(&hrtc, &sAlarm, RTC_FORMAT_BCD);
-        ledOff();
-        // Enter Stop Mode
-        HAL_PWR_EnterSTOPMode(PWR_LOWPOWERREGULATOR_ON, PWR_STOPENTRY_WFI);
-        // Exit Stop Mode
-        ledOn();
-
-        // Update SysTick counter after sleep wake up
-        RTC_TimeTypeDef wakeUpTime = {0};
-        HAL_RTC_GetTime(&hrtc, &wakeUpTime, RTC_FORMAT_BCD);
-        uint32_t sleepMs = wakeUpTime.Hours * 3600 + wakeUpTime.Minutes * 60 + wakeUpTime.Seconds;
-        extern __IO uint32_t uwTick;
-        if (sleepMs < sleepDurationMs) {
-            uwTick += sleepDurationMs - sleepMs;
-        } else {
-            uwTick += sleepDurationMs;
-        }
-
-        if (rtcWakeupFlag) {
-            rtcWakeupFlag = false;
-        } else {
-            HAL_RTC_DeactivateAlarm(&hrtc, sAlarm.Alarm);
-        }
-
-        // Enable SysTick and update clock configuration
-        extern void SystemClock_Config(void);
-        SystemClock_Config();
-        HAL_ResumeTick();
+    if (isSoundPlaying || isKeyScanRunning || isEffectPlaying) {
+        
     } else {
-        __WFI();  // Wait For Interrupt instruction
+        // TODO: fix state transition blocking by sleep, use state machine to handle sleep
+        // enterStopMode();
     }
-//  __enable_irq();
+    // __enable_irq();
     // HAL_IWDG_Refresh(&hiwdg);
 }
