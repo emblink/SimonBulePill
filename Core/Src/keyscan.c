f30f141030f7a433bdd9c3e5b35c4b650d7b02a2
OleksiiTeplyi <oleksiy.tepliy@developex.net>
2025-02-12 21:57:42 -0500

    add key debounce

--- Core/Src/keyscan.c
+++ Core/Src/keyscan.c
@@ -1,16 +1,30 @@
 #include "keyscan.h"
 #include <stddef.h>
+#include "generic.h"
 
 #define US_TIMER 0
 #define PROCESS_COUNT (20)
 #define PRESSED_THRESHOLD (5)
 #define RELEASED_THRESHOLD (-5)
+#define DEBOUNCE_MS (20)
 
 static volatile int processCounter = 0;
 static KeyCallback keyCb = NULL;
 
 static int keyThreshold[KEY_COUNT] = {0};
-static bool keyState[KEY_COUNT] = {false};
+static bool keyIsPressed[KEY_COUNT] = {false};
+static uint32_t pressDebounce[KEY_COUNT] = {0};
+static uint32_t releaseDebounce[KEY_COUNT] = {0};
+
+//  VCC __/ _____ INPUT_PIN
+//            |
+//            |
+//            -
+//           | | Pull Down Resistor
+//            -
+//            |
+//           GND
+// Pressed: 1, Released: 0
 
 // EXTI Line9 External Interrupt ISR Handler CallBackFun
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
@@ -37,25 +51,29 @@ void keyscanProcess()
 
 	for (int key = KEY_RED; key < KEY_COUNT; key++) {
 		GPIO_PinState state = HAL_GPIO_ReadPin(keyTable[key].port, keyTable[key].pin);
-		if (GPIO_PIN_RESET == state) {
+		if (GPIO_PIN_SET == state) {
 			keyThreshold[key]++;
 			if (keyThreshold[key] >= PRESSED_THRESHOLD) {
-				if (true == keyState[key]) {
-					keyState[key] = false;
-                    if (keyCb) {
-                        keyCb(key, keyState[key]);
+                if (!keyIsPressed[key]) {
+                    bool debounced = isTimeoutHappened(pressDebounce[key], DEBOUNCE_MS);
+                    if (keyCb && debounced) {
+                        pressDebounce[key] = HAL_GetTick();
+                        keyIsPressed[key] = true;
+                        keyCb(key, keyIsPressed[key]);   
                     }
-				}
+                }
 			}
 		} else {
 			keyThreshold[key]--;
 			if (keyThreshold[key] <= RELEASED_THRESHOLD) {
-				if (false == keyState[key]) {
-					keyState[key] = true;
-                    if (keyCb) {
-                        keyCb(key, keyState[key]);
+                if (keyIsPressed[key]) {
+                    bool debounced = isTimeoutHappened(releaseDebounce[key], DEBOUNCE_MS);
+                    if (keyCb && debounced) {
+                        releaseDebounce[key] = HAL_GetTick();
+                        keyIsPressed[key] = false;
+                        keyCb(key, keyIsPressed[key]);
                     }
-				}
+                }
 			}
 		}
 	}
